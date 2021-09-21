e8aef971716d4a389956ce5c2eda5d1a6420b0f3
SoulXP <snowflake1194@gmail.com>
2021-09-21 14:48:24 +0200

    added script for basic text loading function

---
+++ scripts/loading.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+load() {
+	tput civis
+    local text="${blue}$2\033[0K\r${reset}"
+    echo -ne $text
+    while kill -0 $1 2>/dev/null; do
+        local dot=""
+        for i in {0..3}; do
+            local text="${blue}$2$dot\033[0K\r${reset}"
+            local dot="$dot."
+            echo -ne $text
+            sleep 0.5
+        done
+	done
+    echo -ne "\033[0K\r"
+	tput cnorm
+}
