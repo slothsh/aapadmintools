2c2bd839b741d952c831d04fe7421e2976c6e830
SoulXP <snowflake1194@gmail.com>
2021-11-22 23:33:01 +0200

    added multiple character search queries

--- scripts/aapgetlines/aapgetlines.sh
+++ scripts/aapgetlines/aapgetlines.sh
@@ -9,23 +9,43 @@ if [[ -v aapadmintools ]]; then
     source ${aapadmintools}/scripts/connectsmb.sh
 fi
 
+# Save IFS
+IFS_=$IFS
+
 # Parse input flags
 report_errors=false
 error_list=()
 re_valid_name="^[A-z0-9]+$"
 re_numbers="^[0-9]+$"
 
-while getopts 'j:u:p:c:l:e:f' arg 2>/dev/null; do
+# Parse input arguments
+while getopts 'j:u:p:c:l:e:s:f' arg 2>/dev/null; do
     case $arg in
         p)
             inpath="$OPTARG";
             # TODO: error reporting
             ;;
         c)
-            character=$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]' | sed -e 's/^[[:space:]]*//g' -e 's/ *$//g')
+            character_list=()
+            re_character_flag="(-c|^)[A-z0-9_, \-]*$"
+            skip=false
+            IFS=","
+            for i in ${@}; do
+                if [[ ${i} =~ $re_character_flag ]]; then
+                    [[ ${i:0:2} =~ "-[^c]" ]] && skip=true
+                    [[ ${i:0:2} = "-c" ]] && i=$(echo $i | sed -e 's/-c//g') && skip=false
+                    [[ $skip = false ]] && for j in $(echo $i | sed -e 's/,$//g' -e 's/,/ /g'); do character_list+=$j; done
+                fi
+            done
+            IFS=$IFS_
 
-            # Error check
-            [[ ! $character =~ $re_valid_name ]] && error_list+="please provide a valid character name with flag -c" && report_errors=true
+            # Ensure that at least 1 character was specified
+            [[ $#character_list[@] = 0 ]] && error_list+="please specify at least one character to search with flag -c" && report_errors=true
+
+            character_list=($(for v in $character_list[@]; do
+                echo $v
+            done | xargs))
+            character_list=($(for v in $character_list[@]; do echo $(echo $v | tr '[:lower:]' '[:upper:]' | sed -e 's/^[[:space:]]*//g' -e 's/ *$//g'); done | sort -n | uniq))
             ;;
 
         l)
@@ -49,27 +69,21 @@ while getopts 'j:u:p:c:l:e:f' arg 2>/dev/null; do
             ;;
 
         e)
-            search_episode="$OPTARG"
-
-            # Error check
-            # if [[ ! $search_episode =~ $re_numbers ]] && error_list+="Please pass a valid number for an episode with flag -e" && report_errors=true
-
             episode_list=()
-            n=0
             re_episode_flag="(-e|^)[0-9]+((,|, +| +|:|$)[0-9]+)*"
-            re_not_eflag="-[^e]"
+            skip=false
+            IFS=","
             for i in ${@}; do
                 if [[ ${i} =~ $re_episode_flag ]]; then
-                    [[ ${i:0:2} = "-e" ]] && i=$(echo $i | sed -e 's/-e//g')
-                    for j in $(echo $i | sed -e 's/,$//g' -e 's/,/ /g'); do
-                        episode_list+=$j
-                    done
-                    n=$(( n + 1 ))
+                    [[ ${i:0:2} =~ "-[^e]" ]] && skip = true
+                    [[ ${i:0:2} = "-e" ]] && i=$(echo $i | sed -e 's/-e//g') && skip = false
+                    [[ $skip = false ]] && for j in $(echo $i | sed -e 's/,$//g' -e 's/,/ /g'); do episode_list+=$j; done
                 fi
             done
+            IFS=$IFS_
 
-            # Ensure that all -e options was captured in one pass
-            [[ $n > 1 ]] && error_list+="please ensure that there are no spaces in -e flag option" && report_errors=true
+            # Ensure that at least 1 episode was specified
+            [[ $#episode_list[@] = 0 ]] && error_list+="please specify at least one episode to search with flag -e" && report_errors=true
 
             episode_list=($(for v in $episode_list[@]; do
                 re_colon='^[0-9]+:[0-9]+$'
@@ -83,22 +97,26 @@ while getopts 'j:u:p:c:l:e:f' arg 2>/dev/null; do
             report_errors=false
             ;;
 
+        s)
+            ;;
+
         :)
-            printf "${_red}Please provide an arguments for flag -${arg}${_reset}\n" >&2
+            printf "${_red}aapgetlines: please provide an arguments for flag -${arg}${_reset}\n" >&2
             exit 1
             ;;
 
         ?)
-            printf "${red:-""}Invalid flag: ${(P)OPTIND}\n${reset:-""}"
+            printf "${red:-""}aapgetlines: no options for flag ${(P)OPTIND}\n${reset:-""}"
             exit 1
             ;;
     esac
 done
+shift $((OPTIND -1))
 
 # Check if essential flags were set
 if [[ ! -v update_cuefiles && ! -v flush_local ]]; then 
-    [[ ! -v character ]] && error_list+="Flag -c must be specified${reset:-""}" && report_errors=true
-    [[ ! -v search_proj ]] && error_list+="Flag -j must be specified" && report_errors=true
+    [[ ! -v character_list ]] && error_list+="flag -c must be specified${reset:-""}" && report_errors=true
+    [[ ! -v search_proj ]] && error_list+="flag -j must be specified" && report_errors=true
 fi
 
 # Error reporting for essential flags
@@ -188,16 +206,25 @@ fi
 count_results=0
 suppress_final_matches=true
 # Prepare episode list & queries for searching
-if [[ $search_episode != "" ]] then file_name="*_ep$search_episode.txt"; else file_name="*.txt"; fi
 total_episodes=$(find "$tmpadmin/aapgetlines/cuefiles/$(echo $search_proj | sed -e 's/ /_/g')" -type f | wc -l)
 [[ ${#episode_list[@]} = 0 ]] && episode_list=($(seq 1 $total_episodes)) && all_search=true && suppress_final_matches=false || all_search=false
 # TODO : Loading text for long searches
 episode_files=($(for n in $episode_list[@]; do find "$tmpadmin/aapgetlines/cuefiles/$(echo $search_proj | sed -e 's/ /_/g')" -name "*_ep$(printf "%02d" $n).txt" -and -type f; done))
 
-printf "${_blue}results for $character in $search_proj:${_reset}\n" >&2
+
+printf "${_blue}results for $character_list in $search_proj:${_reset}\n" >&2
+
+# Build aapgetlines binary query string
+characters=""
+i=0
+for c in $character_list[@]; do
+    (( i < $#character_list[@] - 1 )) && characters+="$c," || characters+="$c"
+    i=$(( i + 1 ))
+done
+
 for f in $episode_files[@]; do
-    [[ $no_colour = true ]] && results=$(echo $f | xargs -I % aapgetlines % $character 2>/dev/null | egrep "$line" --ignore-case --color=never)
-    [[ $no_colour = false ]] && results=$(echo $f | xargs -I % aapgetlines % $character 2>/dev/null | egrep "\b$line\b" --ignore-case --color=${grep_colour})
+    [[ $no_colour = true ]] && results=$(echo $f | xargs -I % aapgetlines % $characters 2>/dev/null | egrep "$line" --ignore-case --color=never)
+    [[ $no_colour = false ]] && results=$(echo $f | xargs -I % aapgetlines % $characters 2>/dev/null | egrep "\b$line\b" --ignore-case --color=${grep_colour})
     # [[ ! $results = "" || $all_search = false ]] && printf "${blue:-""}$(basename $f):${reset:-""}\n"
     [[ ! $results = "" || $all_search = false ]] && printf "${blue:-""}$(sed -e '1q' $f | cut -f 2):${reset:-""}\n"
     [[ $results = "" && $all_search = false ]] && printf "${yellow:-""}no matches${reset:-""}\n\n"
