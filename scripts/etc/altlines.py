4ac17eb83b88ccb0ba4167baa80c0a8420fb2049
SoulXP <snowflake1194@gmail.com>
2021-12-02 15:18:26 +0200

    refactored etc script altlines.py

--- scripts/etc/altlines.py
+++ scripts/etc/altlines.py
@@ -3,84 +3,120 @@
 import os
 import re
 
-def alt_names(name):
+# 1) NADIA - HOPE
+# 2) LEF - LEV / LEO
+# 3) LESHA - LYOSHA
+# 4) ET - YAKOV
+# 5) OLYA - OLGA
+
+def alt_name(name):
     if name.lower() == 'misha':
         return 'Michael'
     elif name.lower() == 'michael':
         return 'Misha'
+    elif name.lower() == 'lev':
+        return 'Leo'
+    elif name.lower() == 'leo':
+        return 'Lev'
     elif name.lower() == 'olga':
         return 'Olya'
     elif name.lower() == 'olya':
         return 'Olga'
-    elif name.lower() == 'leo':
-        return 'Lev'
-    elif name.lower() == 'lev':
-        return 'Leo'
     elif name.lower() == 'et':
         return 'Yakov'
     elif name.lower() == 'yakov':
         return 'Et'
-    elif name.lower() == 'nadia':
-        return 'Hope'
-    elif name.lower() == 'hope':
-        return 'Nadia'
-    elif name.lower() == 'rohit':
-        return 'Rudolph'
-    elif name.lower() == 'rudolph':
-        return 'Rohit'
-    elif name.lower() == 'sonakshi':
-        return 'Kimberleigh'
-    elif name.lower() == 'kimberleigh':
-        return 'Sonakshi'
-    elif name.lower() == 'nishi':
-        return 'Savannah'
-    elif name.lower() == 'savannah':
-        return 'Nishi'
-    elif name.lower() == 'veena':
-        return 'Pauline'
-    elif name.lower() == 'pauline':
-        return 'Veena'
-
+    
     return ''
 
-def name_combinations(text, names_list, i = 0, n = 0, acc = []):
-    acc.append(text)
+def good_name(name):
+    lower_name = name.lower()
 
-    regex_names = '|'.join(names_list)
-    matches_set = set(re.findall(f'\\b({regex_names})\\b', text, flags=re.IGNORECASE))
-    matches = [x for x in matches_set]
+    if lower_name == 'michael':
+        return 'Misha'
+    elif lower_name == 'lev' or lower_name == 'lef':
+        return 'Leo'
+    elif lower_name == 'olga':
+        return 'Olya'
+    elif lower_name == 'et':
+        return 'Yakov'
+    elif lower_name == 'yakov':
+        return 'Et'
+    
+    
+    return ''
 
-    if n == 2 ** len(matches) - 1:
+# c  = 2^i
+#                                                                                              i:c
+# a1           -> a2                                                                         = 1:2
+# a1,b1        -> a2,b1 ; a1,b2 ; a2,b2                                                      = 2:4
+# a1,b1,c1     -> a2,b1,c1 ; a1,b2,c1 ; a1,b1,c2 ; a2,b2,c1 ; a2,b1,c2 ; a1,b2,c2 ; a2,b2,c2 = 3:8
+# a1,b1,c1,d1  -> a2,b1,c1,d1 ; a1,b2,c1,d1 ; a1,b1,c2,d1 ; a1,b1,c1,d2
+#               ; a2,b2,c1,d1 ; a2,b1,c2,d1 ; a2,b1,c1,d2 ; a1,b2,c2,d1
+#               ; a1,b2,c1,d2 ; a2,b2,c2,d1 ; a2,b2,c1,d2 ; a1,b1,c2,d2
+#               ; a2,b1,c2,d2 ; a1,b2,c2,d2 ; a2,b2,c2,d2                                    = 4:8
+
+# 0 0 0 0 = 0
+# 0 0 0 1 = 1
+# 0 0 1 0 = 2
+# 0 0 1 1 = 3
+# 0 1 0 0 = 4
+# 0 1 0 1 = 5
+# 0 1 1 0 = 6
+# 0 1 1 1 = 7
+# 1 0 0 0 = 8
+# 1 0 0 1 = 9
+# 1 0 1 0 = 10
+# 1 0 1 1 = 11
+# 1 1 0 0 = 12
+# 1 1 0 1 = 13
+# 1 1 1 0 = 14
+# 1 1 1 1 = 15
+
+# pseudo:
+#   pop index 0
+#   invert index 0
+#   append index 0 at index 0
+#
+
+def name_combinations(line, i = 0, n = 0, search = [], acc = []):
+    acc.append(line)
+
+    re_names = 'nadia|hope|misha|michael|lev|leo|lesha|lyosha|et|yakov|olya|olga'
+    n_names = len(set(re.findall(f'\\b({re_names})\\b', line, flags=re.IGNORECASE)))
+
+    if not search:
+        matches = set(re.findall(f'\\b({re_names})\\b', line, flags=re.IGNORECASE))
+        search = [x for x in matches]
+        search.sort()
+
+    if i == len(search):
+        print(f'{i}')
+        i = 0
+    if n == 2 ** n_names - 1:
         return acc
 
-    if i == len(matches): i = 0
-    new_line = re.sub(matches[i], alt_names(matches[i]), text, flags=re.IGNORECASE)
+    print(f'{search}')
+    alt = search.pop()
+    new_line = re.sub(f'\\b({alt})\\b', alt_name(alt), line, flags=re.IGNORECASE)
 
-    return name_combinations(new_line, names_list, i + 1, n + 1, acc)
+
+    return name_combinations(new_line, i + 1, n + 1, search, acc)
+
+def name_replace():
+    re_names = 'nadia|michael|lev|lef|lesha|lyosha|et|olya'
 
 def main():
-    out_results = []
-
-    with open('/mnt/c/users/snowf/dev/projects/aapadmintools/resources/altlines_test.txt', 'r') as f:
+    with open('/Users/stefan/Desktop/hopealtnames/test.tab') as f:
         lines = f.readlines()
-        names_list = ['misha', 'michael', 'et', 'yakov', 'nadia', 'hope', 'rohit', 'rudolph', 'sonakshi', 'kimberleigh', 'nishi', 'savannah', 'veena', 'pauline']
 
         for line in lines:
             fields = line.split(sep='\t')
-            text = fields[4].strip()
-            alt_text = name_combinations(text, names_list)
-            append_text = '\t'.join(alt_text)
-            new_line = f'{fields[0]}\t{fields[1]}\t{fields[2]}\t{fields[3]}\t{append_text}\n'
-            # new_line = f'{append_text}\n'
-            print(new_line)
-            out_results.append(new_line)
-            alt_text.clear()
-
-    # with open('/mnt/c/users/snowf/dev/projects/aapadmintools/resources/etelov_alt.txt', 'w') as f:
-    #     for line in out_results:
-    #         f.write(line)
-
-    #     f.close()
+            original = fields[4].strip()
+            new = re.sub(alt_name)
+            combinations = name_combinations(original)
+                    
+        print(combinations)
 
 if __name__ == '__main__':
     main()
