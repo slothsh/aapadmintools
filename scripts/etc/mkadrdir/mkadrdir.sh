12f084e1f599994d0ac3693d60f5d500720b5227
SoulXP <snowflake1194@gmail.com>
2022-09-12 17:44:06 +0200

    modifed adr-mkdir to pre-check directories and files modified adr-mkdir to move xls and tab files

--- scripts/etc/mkadrdir/mkadrdir.sh
+++ scripts/etc/mkadrdir/mkadrdir.sh
@@ -29,11 +29,44 @@ if [[ ${#files[@]} -eq 0 ]]; then
 fi 
 
 ver=1
+proj_name=$(echo $proj_name | tr '[:lower:]' '[:upper:]')
+proj_code=$(echo $proj_code | tr '[:lower:]' '[:upper:]')
+
 for f in $files; do
     v0="P_${proj_name} [${proj_code}] {EP${f} V0}"
     v1="${v0}/P_${proj_name} [${proj_code}] {EP${f} V1}"
     adr_files="${v1}/G_${proj_code}_Ep${f}_ADR_Files"
+    xls_file="G_${proj_code}_Ep${f}_ADR_Text_Excel_Files"
     ptx_file="G_${proj_code}_Ep${f}_ADR_PTX_File"
-    mkdir "./${v0}" && mkdir "./${v1}" && mkdir "./${adr_files}"
-    mv "./${ptx_file}" "./${adr_files}"
+    tab_file="${proj_code}_EP${f}.gen.TAB"
+
+    if [[ ! -d "./${v0}" ]]; then
+        mkdir "./${v0}"
+        # printf "./${v0}\n"
+    fi
+
+    if [[ ! -d "./${v1}" ]]; then
+        mkdir "./${v1}"
+        # printf "./${v1}\n"
+    fi
+
+    if [[ ! -d "./${adr_files}" ]]; then
+        mkdir "./${adr_files}"
+        # printf "./${adr_files}\n"
+    fi
+
+    if [[ -d "./${xls_file}" ]]; then
+        mv "./${xls_file}" "./${adr_files}"
+        # printf "./${xls_files}\n"
+    fi
+
+    if [[ -d "./${ptx_file}" ]]; then
+        mv "./${ptx_file}" "./${adr_files}"
+        # printf "./${xls_files}\n"
+    fi
+
+    if [[ -f "./${tab_file}" ]]; then
+        mv "./${tab_file}" "./${adr_files}"
+        # printf "./${xls_files}\n"
+    fi
 done
